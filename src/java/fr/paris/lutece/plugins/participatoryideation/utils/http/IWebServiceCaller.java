3e29948494dfa1eb88beeb748215a537856f4aad
Clara Girard <clara.girard@epitech.eu>
2019-12-03 18:12:28 +0100

    PARTIDEA-9 : Plug to campaign WS-REST API of plugin-participatifbudget

---
+++ src/java/fr/paris/lutece/plugins/participatoryideation/utils/http/IWebServiceCaller.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2002-2014, Mairie de Paris
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice
+ *     and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice
+ *     and the following disclaimer in the documentation and/or other materials
+ *     provided with the distribution.
+ *
+ *  3. Neither the name of 'Mairie de Paris' nor 'Lutece' nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * License 1.0
+ */
+package fr.paris.lutece.plugins.participatoryideation.utils.http;
+
+import fr.paris.lutece.util.signrequest.RequestAuthenticator;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * WebServiceCaller Interface
+ *
+ */
+public interface IWebServiceCaller
+{
+    /**
+     * This method calls Rest WS to do an action
+     * 
+     * @param strUrl
+     *            the url
+     * @param mapParameters
+     *            the params to pass in the post
+     * @param authenticator
+     *            The request authenticator
+     * @param listElements
+     *            the list of elements to include in the signature
+     * @param httpMethod
+     *            the http method used
+     * @return the response as a string
+     * @throws CRMException
+     *             the exception if there is a problem
+     */
+    String callWebService( String strUrl, Map<String, String> mapParameters, RequestAuthenticator authenticator, List<String> listElements,
+            HttpMethodEnum httpMethod ) throws Exception;
+}
