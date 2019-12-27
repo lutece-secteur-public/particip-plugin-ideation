1d0fc3732618c3434d7b2111d0f93ac4cb779aaf
Jerome Poirier <jerome.poirier@paris.fr>
2019-12-27 12:12:40 +0100

    PARTIDEA-16 : i18n on idea sheet

---
+++ src/sql/plugins/participatoryideation/upgrade/update_db_core_participatoryideation-1.0.0-1.1.0.sql
@@ -0,0 +1,7 @@
+INSERT INTO `core_datastore` (`entity_key`, `entity_value`) VALUES ('participatoryideation.site_property.view_idee.non_existing.htmlblock', '<p class="text-center">No such proposal.</p>');
+INSERT INTO `core_datastore` (`entity_key`, `entity_value`) VALUES ('participatoryideation.site_property.view_idee.site_properties.contact_message_content.textblock', '${nom_depositaire}, ${nom_contacteur}, ${message}');
+INSERT INTO `core_datastore` (`entity_key`, `entity_value`) VALUES ('participatoryideation.site_property.view_idee.site_properties.contact_message_not_accept', 'Unable to sent the message.');
+INSERT INTO `core_datastore` (`entity_key`, `entity_value`) VALUES ('participatoryideation.site_property.view_idee.site_properties.contact_subject', 'contact submitter');
+INSERT INTO `core_datastore` (`entity_key`, `entity_value`) VALUES ('participatoryideation.site_property.view_my_subscriptions.buttonSave', 'Save my subscriptions');
+INSERT INTO `core_datastore` (`entity_key`, `entity_value`) VALUES ('participatoryideation.site_property.view_my_subscriptions.message.textblock', 'I want to receive a notification about this events :');
+INSERT INTO `core_datastore` (`entity_key`, `entity_value`) VALUES ('participatoryideation.site_property.view_my_subscriptions.title.textblock', 'My notifications');
