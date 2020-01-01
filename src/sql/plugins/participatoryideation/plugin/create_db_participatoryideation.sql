61b3bfd1679fb2580373a1df8ef0c624b6890e5d
Jérôme Poirier <jerome.poirier@paris.fr>
2020-01-01 22:01:37 +0100

    PARTIDEA-33 : Unexpected engine specification in SQL files

--- src/sql/plugins/participatoryideation/plugin/create_db_participatoryideation.sql
+++ src/sql/plugins/participatoryideation/plugin/create_db_participatoryideation.sql
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS `ideation_depositaire_complement_types` (
   `libelle` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id_depositaire_complement_type`),
   UNIQUE KEY `code_depositaire_complement_type` (`code_depositaire_complement_type`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+);
 
 CREATE TABLE IF NOT EXISTS `ideation_depositaire_types` (
   `id_depositaire_type` int(6) NOT NULL,
@@ -25,7 +25,7 @@ CREATE TABLE IF NOT EXISTS `ideation_depositaire_types` (
   UNIQUE KEY `code_depositaire_type` (`code_depositaire_type`),
   KEY `fk_ideation_depositaire_types_complement` (`code_complement_type`),
   CONSTRAINT `fk_ideation_depositaire_types_complement` FOREIGN KEY (`code_complement_type`) REFERENCES `ideation_depositaire_complement_types` (`code_depositaire_complement_type`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+);
 
 CREATE TABLE IF NOT EXISTS `ideation_depositaire_types_values` (
   `id_depositaire_type_value` int(6) NOT NULL,
@@ -35,7 +35,7 @@ CREATE TABLE IF NOT EXISTS `ideation_depositaire_types_values` (
   PRIMARY KEY (`id_depositaire_type_value`),
   UNIQUE KEY `code_depositaire_type` (`code_depositaire_type`,`code`),
   CONSTRAINT `fk_ideation_depositaire_type_values_depositaire` FOREIGN KEY (`code_depositaire_type`) REFERENCES `ideation_depositaire_types` (`code_depositaire_type`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+);
 
 CREATE TABLE IF NOT EXISTS `ideation_campagnes_depositaires` (
   `id_campagne_depositaire` int(6) NOT NULL,
@@ -45,7 +45,7 @@ CREATE TABLE IF NOT EXISTS `ideation_campagnes_depositaires` (
   KEY `fk_ideation_campagnes_depositaires_campagne` (`code_campagne`),
   KEY `fk_ideation_campagnes_depositaires_depositaire` (`code_depositaire_type`),
   CONSTRAINT `fk_ideation_campagnes_depositaires_depositaire` FOREIGN KEY (`code_depositaire_type`) REFERENCES `ideation_depositaire_types` (`code_depositaire_type`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+);
 
 CREATE TABLE IF NOT EXISTS `ideation_idees` (
   `id_idee` int(6) NOT NULL,
@@ -84,7 +84,7 @@ CREATE TABLE IF NOT EXISTS `ideation_idees` (
   `handicap_complement` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id_idee`),
   UNIQUE KEY `code_campagne` (`code_campagne`,`code_idee`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+);
 
 CREATE TABLE IF NOT EXISTS `ideation_idees_files` (
   `id_idee_file` int(6) NOT NULL,
@@ -95,7 +95,7 @@ CREATE TABLE IF NOT EXISTS `ideation_idees_files` (
   KEY `id_idee` (`id_idee`,`type`,`id_file`),
   KEY `id_file` (`id_file`),
   CONSTRAINT `ideation_idees_files_idee` FOREIGN KEY (`id_idee`) REFERENCES `ideation_idees` (`id_idee`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+);
 
 CREATE TABLE IF NOT EXISTS `ideation_idees_links` (
   `id_idee_link` int(6) NOT NULL,
@@ -104,13 +104,13 @@ CREATE TABLE IF NOT EXISTS `ideation_idees_links` (
   PRIMARY KEY (`id_idee_link`),
   UNIQUE KEY `id_idee_child` (`id_idee_child`,`id_idee_parent`),
   KEY `id_idee_parent` (`id_idee_parent`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+);
 
 CREATE TABLE IF NOT EXISTS `task_change_idee_status_cf` (
   `id_task` int(11) NOT NULL,
   `idee_status` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id_task`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+);
 
 CREATE TABLE IF NOT EXISTS `task_notify_ideation_cf` (
   `id_task` int(11) NOT NULL,
@@ -123,4 +123,4 @@ CREATE TABLE IF NOT EXISTS `task_notify_ideation_cf` (
   `isFollowers` smallint(6) NOT NULL,
   `isDepositaire` smallint(6) NOT NULL,
   PRIMARY KEY (`id_task`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+);
