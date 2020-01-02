93396e31c9f326c3b59cbf04899b46e5c97ac586
Jerome Poirier <jerome.poirier@paris.fr>
2020-01-02 15:46:53 +0100

    PARTIDEA-35 : Fix Sonar issues

--- src/java/fr/paris/lutece/plugins/participatoryideation/service/IdeationStaticService.java
+++ src/java/fr/paris/lutece/plugins/participatoryideation/service/IdeationStaticService.java
@@ -86,14 +86,17 @@ public class IdeationStaticService extends AbstractCacheableService implements I
     public void fillCampagneStaticContent(Map<String, Object> model, String strCampagneCode) {
         @SuppressWarnings("unchecked")
         Map<String, Object> cached = (Map<String, Object>) getFromCache( CACHE_KEY );
-        if (cached == null) {
+        
+        if ( cached == null ) 
+        {
            cached = putAllStaticContentInCache(  ); 
         }
+        
         Collection<Campagne> listCampagne = CampagneHome.getCampagnesList();
-        if(listCampagne!=null && listCampagne.size()>0)
+        if ( listCampagne!=null && listCampagne.size() > 0 )
         {
         	model.put( MARK_LIST_CAMPAGNE, listCampagne );
-        }
+        }	id
         model.put( MARK_CAMPAGNE_STATIC, cached.get(strCampagneCode) );
         model.put( MARK_ARRONDISSEMENTS_LIST, IdeeService.getInstance().getArrondissements() );
         model.put( MARK_ARRONDISSEMENTS_MAP, IdeeService.getInstance().getArrondissementsMap() );
